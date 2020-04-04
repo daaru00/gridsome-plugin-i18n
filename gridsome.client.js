5f13a0d3bd71276321c055c5d19b66ea7b7533c9
Fabio Gollinucci <fbgolly@gmail.com>
2020-04-04 13:48:42 +0200

    Fix path rewrite on already localized path

--- gridsome.client.js
+++ gridsome.client.js
@@ -33,13 +33,19 @@ export default function (Vue, options, { appOptions, router }) {
 
   // Maintain path prefix during router change
   router.beforeResolve((to, from, next) => {
-    // if option is disabled skip logic
+    // do not rewrite build paths
+    if (process.isServer) {
+      next()
+      return 
+    }
+
+    // if option is disabled, skip whole logic
     if (options.enablePathRewrite === false) {
       next()
       return 
     }
 
-    // if is first page load skip logic
+    // if is first page load, skip whole logic
     const currentLocale = i18n.locale
     if (!currentLocale) {
       next()
@@ -47,18 +53,26 @@ export default function (Vue, options, { appOptions, router }) {
     }
 
     // Elaborate path prefix
-    const pathSegment = options.pathAliases[currentLocale] || currentLocale
     let pathToResolve = to.path || '/'
     if (!pathToResolve.startsWith('/')) {
       pathToResolve = '/' + pathToResolve
     }
+    const pathSegment = options.pathAliases[currentLocale] || currentLocale
     const pathPrefix = '/' + pathSegment
 
     // if path already contain path prefix skip rewrite
-    if (pathToResolve.startsWith(pathPrefix)) {
+    const pathToResolveSegments = pathToResolve.split('/')
+    const pathToResolveLocale = options.locales.find(locale => {
+      const pathSegment = options.pathAliases[locale] || locale
+      // First element is an empty string, second is the first path segment
+      return pathToResolveSegments[1] === pathSegment
+    })
+    
+    // Path already contain a valid locale prefix, skip rewrite
+    if (pathToResolveLocale !== undefined) {
       next()
       return
-    }    
+    }
 
     // Rewrite path
     next({
